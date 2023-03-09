5f8d9561663110783916c0d07b0cf9853eff5ca6
101cade <118102920+101cade@users.noreply.github.com>
2023-03-09 08:50:30 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace NumericTypes
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            double length;
+            double width;
+            double area;
+            string input; 
+
+
+            Console.WriteLine("Length: ");
+            input = Console.ReadLine();
+            length = double.Parse(input);
+
+            Console.WriteLine("Width: ");
+            input = Console.ReadLine();
+            width = double.Parse(input);
+
+            area = (length * width);
+            Console.WriteLine("The area of your rectangle is: " + area);
+            Console.ReadLine();
+
+        }
+    }
+}
